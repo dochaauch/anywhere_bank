7d72dd93d8f4e905a337b20c946b7e80f6f8b569
Jelena Jakobson <lena.docha@gmail.com>
2023-06-04 19:41:35 +0300

    refactor: update translateString

--- processing.py
+++ processing.py
@@ -136,5 +136,7 @@ def translateString(our_string):
     special_char_map = {ord('ä'): 'a', ord('ü'): 'u', ord('ö'): 'o', ord('õ'): 'o',
                         ord('ž'): 'z', ord('š'): 's',
                         ord('Ä'): 'A', ord('Ü'): 'U', ord('Ö'): 'O', ord('Õ'): 'O',
-                        ord('Z'): 'Z', ord('Š'): 's', ord('’'): ''}
+                        ord('Z'): 'Z', ord('Š'): 'S', ord('’'): '',
+                        ord('Ł'): 'L',
+                        }
     return our_string.translate(special_char_map)
